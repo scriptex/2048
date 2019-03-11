48926a1f992746f65aa2cb25f6f327747cc4f3e8
Atanas Atanasov <scriptex.bg@gmail.com>
2019-03-11 09:09:48 +0200

    Register the service worker

--- assets/scripts/main.ts
+++ assets/scripts/main.ts
@@ -1,3 +1,9 @@
 import Game from './game';
 
 window.requestAnimationFrame(() => new Game(4));
+
+if ('serviceWorker' in navigator) {
+	window.addEventListener('load', () => {
+		navigator.serviceWorker.register('./service-worker.js');
+	});
+}
